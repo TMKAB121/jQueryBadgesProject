a1d413ed7ee9114109b1ddb3daccf537de76eb1b
Anthony Sayge <TMKAB121@gmail.com>
2017-07-13 16:50:39 -0500

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,16 +4,14 @@ $(function() {
 	  success: function(response){
 		  var completed_courses = response.courses.completed;
 		  var length = completed_courses.length;
-		  var build_string;
+		  var build_string = '';
 		  for(var i=0; i < length; i++){
-        if( completed_courses[i] !== undefined ){
-          build_string += '<div class="course">';
-          build_string += '<h3>' + completed_courses[i]['title'] + '</h3>';
-          build_string += '<img alt="" src="' + completed_courses[i]['badge'] + '"/>';
-          build_string += '<a href="' + completed_courses[i]['url'] + '" target="_blank" class="btn btn-primary">See Course</a>';
-          build_string += '</div>';
-        }
-		  };
+			  build_string += '<div class="course">';
+			  build_string += '<h3>' + completed_courses[i]['title'] + '</h3>';
+			  build_string += '<img alt="" src="' + completed_courses[i]['badge'] + '"/>';
+			  build_string += '<a href="' + completed_courses[i]['url'] + '" target="_blank" class="btn btn-primary">See Course</a>';
+			  build_string += '</div>';
+		  }
 		  $('#badges').append(build_string);
 	  }
   });
